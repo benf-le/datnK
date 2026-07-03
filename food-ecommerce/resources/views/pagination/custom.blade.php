a3c712365db91700e8106d58627ddfa9d2f0d0a8
benf-le <banglebang2016@gmail.com>
2026-07-03 10:25:24 +0700

    docs: add project thesis documentation and implement admin interface styling and custom pagination components

--- food-ecommerce/resources/views/pagination/custom.blade.php
+++ food-ecommerce/resources/views/pagination/custom.blade.php
@@ -1,25 +1,57 @@
 @if ($paginator->hasPages())
-    <ul class="pagination">
-        @foreach ($elements as $element)
-            @if (is_string($element))
-                <li class="page-item disabled">
-                    <span class="page-link">{{ $element }}</span>
-                </li>
-            @endif
+    <div class="dataTables_wrapper">
+        <div class="dataTables_length">
+            <label>
+                Showing {{ $paginator->firstItem() ?? 0 }} to {{ $paginator->lastItem() ?? 0 }} of {{ $paginator->total() }} entries
+            </label>
+        </div>
+        <div class="dataTables_paginate paging_simple_numbers">
+            <ul class="pagination">
+                {{-- Previous Page Link --}}
+                @if ($paginator->onFirstPage())
+                    <li class="page-item disabled">
+                        <span class="page-link">Previous</span>
+                    </li>
+                @else
+                    <li class="page-item">
+                        <a class="page-link" href="{{ $paginator->previousPageUrl() }}">Previous</a>
+                    </li>
+                @endif
 
-            @if (is_array($element))
-                @foreach ($element as $page => $url)
-                    @if ($page == $paginator->currentPage())
-                        <li class="page-item active">
-                            <span class="page-link">{{ $page }}</span>
-                        </li>
-                    @else
-                        <li class="page-item">
-                            <a class="page-link" href="{{ $url }}">{{ $page }}</a>
+                {{-- Pagination Elements --}}
+                @foreach ($elements as $element)
+                    @if (is_string($element))
+                        <li class="page-item disabled">
+                            <span class="page-link">{{ $element }}</span>
                         </li>
                     @endif
+
+                    @if (is_array($element))
+                        @foreach ($element as $page => $url)
+                            @if ($page == $paginator->currentPage())
+                                <li class="page-item active">
+                                    <span class="page-link">{{ $page }}</span>
+                                </li>
+                            @else
+                                <li class="page-item">
+                                    <a class="page-link" href="{{ $url }}">{{ $page }}</a>
+                                </li>
+                            @endif
+                        @endforeach
+                    @endif
                 @endforeach
-            @endif
-        @endforeach
-    </ul>
+
+                {{-- Next Page Link --}}
+                @if ($paginator->hasMorePages())
+                    <li class="page-item">
+                        <a class="page-link" href="{{ $paginator->nextPageUrl() }}">Next</a>
+                    </li>
+                @else
+                    <li class="page-item disabled">
+                        <span class="page-link">Next</span>
+                    </li>
+                @endif
+            </ul>
+        </div>
+    </div>
 @endif
